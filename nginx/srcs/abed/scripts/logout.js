56b32b7f5f2bf01695476a2ac13e496663bc551e
Your Name <you@example.com>
2024-10-06 16:15:41 +0100

    done but there is issue in front status

--- nginx/srcs/abed/scripts/logout.js
+++ nginx/srcs/abed/scripts/logout.js
@@ -2,7 +2,7 @@ import { get_csrf_token } from "./register.js";
 
 export const logoutBtn = document.querySelector("#logout");
 
-const logoutFuntion = async (event) => {
+export const logoutFuntion = async (event) => {
     event.preventDefault();
     try {
         const token = await get_csrf_token();
@@ -31,13 +31,14 @@ import { singIn_function } from "./login.js";
 
 export const showLogin = ()=> {
     document.querySelector("#login-parent").style.display = "flex";
+    document.querySelector("#full-container").style.display = "none";
     singIn_function();
     // document.querySelector("#online-friends").style.display = "none";
-    document.querySelector("#nav").style.display = "none";
-    document.querySelector("#main").style.display = "none";
-    document.querySelector("#profile-part").style.display = "none";
-    document.querySelector("#chat-part").style.display = "none";
-    document.querySelector("#setting-part").style.display = "none";
-    document.querySelector("#friends-part").style.display = "none";
-    document.querySelector("#rank-part").style.display = "none";
+    // document.querySelector("#nav").style.display = "none";
+    // document.querySelector("#main").style.display = "none";
+    // document.querySelector("#profile-part").style.display = "none";
+    // document.querySelector("#chat-part").style.display = "none";
+    // document.querySelector("#setting-part").style.display = "none";
+    // document.querySelector("#friends-part").style.display = "none";
+    // document.querySelector("#rank-part").style.display = "none";
 }
--- todo.md
+++ todo.md
@@ -1,3 +1,3 @@
-check if the email exists.
-
-should also modify on the status of user in database 
+<!-- logout for mobile. --> done.
+<!-- when logout the login page become bad. --> done.
+need to handle canel after.
